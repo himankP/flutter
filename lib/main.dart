e5773c2a6b910792db2ff6a810c1122f102e144d
Himank Pant <panthimank46@gmail.com>
2025-08-14 11:06:33 +0530

    Implement main function and create MyApp widget for the Sample App

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
-void main(List<String> args) {}
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sample App Home Page')),
+        body: const Center(child: Text('Welcome to the Sample App!')),
+      ),
+    );
+  }
+}
