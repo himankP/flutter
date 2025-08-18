a4970a134aacbdd346ea5b3bfed871155d55bb50
Himank Pant <panthimank46@gmail.com>
2025-08-18 10:15:43 +0530

    Refactor Welcomescreen layout for improved structure and styling

--- lib/screens/Welcomescreen.dart
+++ lib/screens/Welcomescreen.dart
@@ -4,6 +4,31 @@ class Welcomescreen extends StatelessWidget {
   const Welcomescreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text("asdas"), Image.asset("/images/image3.jpeng")]);
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Center(
+        child: Column(
+          children: [
+            Text("asdas"),
+
+            Image.asset("images/image3.jpeg", width: 200, height: 200, fit: BoxFit.cover),
+            SizedBox(height: 20),
+
+            Text("wecomletothe ricpen", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text(
+              "browse delicious recpies from round the world",
+              style: TextStyle(fontSize: 16, color: Colors.blue[600]),
+            ),
+            TextButton(
+              onPressed: () {},
+
+              child: Text("Get Started", style: TextStyle(fontSize: 20, color: Colors.green)),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
